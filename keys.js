e7d75a0e7219953ea45bded91eae4d2163d9f1a2
Dowon Lee <dowonlee777@gmail.com>
2021-04-18 15:51:26 -0400

    files

---
+++ keys.js
@@ -0,0 +1,6 @@
+	// User Bing maps key, to prevent using Cesium default key, fixes Bing maps problem
+	Cesium.BingMapsApi.defaultKey = 'AoKl5Cv_PJg6jo6n8UXVyYSJ6j-HzBfDpBXAtucNBNJ05bPXu96UGBdybzLiTiZO';
+
+    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhN2M5ZDE2NS1mNTk2LTRhMDQtODJlNC02N2Y2YTk1ZDEyYWUiLCJpZCI6NDYzOTEsImlhdCI6MTYxNjA4NzA0N30.9J2ARgw2tUpwlbohrI2kYyGOakLmnS8BNzfBm1ELN5M';
+
+	here_api_key = 'O9Hg9mex8CfcyrMbryKAtSANkpNQAZ_yjUKDyapEEmY';
