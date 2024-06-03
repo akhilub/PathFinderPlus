398b3ce939804254c14df19b90d587f3f3a513d1
Akhil Singh Chauhan <akhilsin@buffalo.edu>
2024-06-03 14:02:57 -0400

    resolved route endpoint

--- scripts/keys.js
+++ scripts/keys.js
@@ -1,6 +1,6 @@
-	// User Bing maps key, to prevent using Cesium default key, fixes Bing maps problem
-	Cesium.BingMapsApi.defaultKey = 'AoKl5Cv_PJg6jo6n8UXVyYSJ6j-HzBfDpBXAtucNBNJ05bPXu96UGBdybzLiTiZO';
+// User Bing maps key, to prevent using Cesium default key, fixes Bing maps problem
+Cesium.BingMapsApi.defaultKey = 'AoKl5Cv_PJg6jo6n8UXVyYSJ6j-HzBfDpBXAtucNBNJ05bPXu96UGBdybzLiTiZO';
 
-    Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhN2M5ZDE2NS1mNTk2LTRhMDQtODJlNC02N2Y2YTk1ZDEyYWUiLCJpZCI6NDYzOTEsImlhdCI6MTYxNjA4NzA0N30.9J2ARgw2tUpwlbohrI2kYyGOakLmnS8BNzfBm1ELN5M';
+Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhN2M5ZDE2NS1mNTk2LTRhMDQtODJlNC02N2Y2YTk1ZDEyYWUiLCJpZCI6NDYzOTEsImlhdCI6MTYxNjA4NzA0N30.9J2ARgw2tUpwlbohrI2kYyGOakLmnS8BNzfBm1ELN5M';
 
-	here_api_key = 'O9Hg9mex8CfcyrMbryKAtSANkpNQAZ_yjUKDyapEEmY';
+here_api_key = 'IGcW2eGwAa271al44ImXq94P31jUGbY9xafLw4rPbPk';
